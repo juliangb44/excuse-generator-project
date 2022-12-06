ce91be4bfea29c168ef12cd2fe8be785e0fc9a44
juliangb44 <juliangb44@gmail.com>
2022-12-06 13:07:34 +0000

    Finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,44 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const who = [
+  "Esperanza Aguirre",
+  "Iron Man",
+  "Darth Vader",
+  "Superman",
+  "Belen Esteban"
+];
+const action = ["eat", "attack me", "infect me", "get me drunk", "kidnap me"];
+const what = ["my code", "my project", "my computer", "my food", "my resume"];
+const when = [
+  "this morning",
+  "yesterday",
+  "5 minutes ago",
+  "at night",
+  "the day after tomorrow"
+];
+const where = [
+  "at the capitol.",
+  "in the bar downstairs.",
+  "in the subway.",
+  "at Hogwarts.",
+  "in my house."
+];
+const listOptions = [who, action, what, when, where];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = generateExcuses();
+  });
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function getRandomElementOfAnArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+let generateExcuses = () => {
+  let excuses = " ";
+
+  for (var i = 0; i < listOptions.length; i++) {
+    excuses = excuses.concat(" ", getRandomElementOfAnArray(listOptions[i]));
+  }
+
+  return excuses;
 };
